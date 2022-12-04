540f931c2f13e94ca1e220409b2bd185287edf48
R1ppy <61470982+R1ppy@users.noreply.github.com>
2022-12-04 18:32:01 -0500

    Add files via upload

---
+++ Morse Code LED Message/Morse Code LED Message.ino
@@ -0,0 +1,160 @@
+int Light = 12;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(Light, OUTPUT);
+digitalWrite(Light, HIGH);
+}
+
+void loop() {
+  //Create Morse code with the pattern of the LED on the breadBoard
+digitalWrite(Light, HIGH);
+delay(10000);
+digitalWrite(Light, LOW);
+delay(10000);
+//Letter H
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+//Letter E
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+//Letter L
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(200);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+// Letter L again
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(200);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+// Letter O
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+//Space
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+//Letter A
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+//Next Letter
+digitalWrite(Light, LOW);
+delay(5000);
+//Letter J
+digitalWrite(Light, HIGH);
+delay(500);
+digitalWrite(Light, LOW);
+delay(500);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+digitalWrite(Light, HIGH);
+delay(3000);
+digitalWrite(Light, LOW);
+delay(300);
+digitalWrite(Light, HIGH)
+}
